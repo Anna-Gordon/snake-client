8ea3b3a9634a4945d2852959a5a1447d45b04dc4
Anna Gordon <annetta@email.com>
2020-02-12 23:35:46 +0000

    Stdin Setup

--- play.js
+++ play.js
@@ -3,4 +3,42 @@ const net = require('net');
 const { connect } = require('./client');
 
 console.log('Connecting ... ');
-connect();
+let connection = connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = (data) => {
+  if ( data === 'i') {
+    connection.write('Move: up'); 
+  }
+  if ( data === 'j') {
+    connection.write('Move: left'); 
+  }
+  if ( data === 'l') {
+    connection.write('Move: right'); 
+  }
+  if ( data === 'k') {
+    connection.write('Move: down'); 
+  }
+
+  if (data === '\\q\n') {
+    connection.end();
+    process.exit();
+  } 
+
+  connection.write(data);
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+
+setupInput();
