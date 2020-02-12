86124ae921087d23451f7eaad478f10126694ec0
Anna Gordon <annetta@email.com>
2020-02-12 22:48:23 +0000

    Move Commands

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const net = require('net');
 
+const moveArr = ["Move: down", "Move: right", "Move: up", "Move: left"];
 const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
@@ -12,6 +13,16 @@ const connect = function() {
     console.log('Successfully connected to game server');
     conn.write('Name: AnG');
   });
+
+  // let whichMovementIndex = 0;
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * moveArr.length);
+    conn.write(moveArr[randomIndex]);
+    // if (whichMovementIndex === moveArr.length) {
+    //   whichMovementIndex = 0;
+    // }
+  }, 500);
+  
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
